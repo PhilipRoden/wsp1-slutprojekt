da94229ae71a681826fe48c3c34d34bf2ac60580
PhilipRoden <philip.roden@elev.ga.ntig.se>
2025-04-20 19:25:56 +0200

    04-20

--- app.rb
+++ app.rb
@@ -1,31 +1,44 @@
 class App < Sinatra::Base
+    enable :sessions
+
     def db
         return @db if @db
-
         @db = SQLite3::Database.new("db/WEB.sqlite")
         @db.results_as_hash = true
         return @db
     end
 
     get '/' do
+        
         redirect('/placeholder')
     end
 
     get '/placeholder' do
+        session[:time] = 60
+        session[:score] = 0
         @leaders = db.execute('SELECT * FROM leaders')
         erb(:"placeholder/index")
     end
 
     get '/placeholder/new' do
+        session[:time] = 60
+        session[:score] = 0
         erb(:"placeholder/new")
     end
 
     get '/placeholder/game' do
-        @leaders = db.execute('SELECT * FROM leaders ORDER BY RANDOM()')
-        erb(:"placeholder/game")
+        @TIME=session[:time]
+        if session[:user_id]
+            @leaders = db.execute('SELECT * FROM leaders ORDER BY RANDOM()')
+            erb(:"placeholder/game")
+          else
+            redirect '/login'
+          end
     end
 
     post '/placeholder' do
+        session[:time] = 60
+        session[:score] = 0
         p params
         name = params["leader_name"]
         country = params["leader_country"]
@@ -35,16 +48,35 @@ class App < Sinatra::Base
         redirect "/placeholder"
     end
 
-    get '/new_user' do 
+    get '/new_user' do
+        session[:time] = 60
+        session[:score] = 0
         erb(:"/new_user")
     end
 
-    get '/placeholder/result' do 
+    get '/placeholder/result' do
         @leaders = db.execute('SELECT * FROM leaders ORDER BY RANDOM()')
+        @score = session[:score]
+        p session[:user_id]
+        p @score
+        score = params[""]
+        if db.execute('SELECT score FROM users WHERE id = ?', session[:user_id]).first['score']== nil
+            db.execute('UPDATE users SET score = ? WHERE id = ?', [@score, session[:user_id]])
+            @message = "New High Score!"
+        elsif @score >= db.execute('SELECT score FROM users WHERE id = ?', session[:user_id]).first['score']
+            db.execute('UPDATE users SET score = ? WHERE id = ?', [@score, session[:user_id]])
+            @message = "New High Score!"
+        else
+            @message = "Try again!"
+        end
+        session[:score] = 0
+        session[:time] = 60
         erb(:"/placeholder/result")
     end
     
     post '/new_user' do
+        session[:score] = 0
+        session[:time] = 60
         p params
         password_hashed = BCrypt::Password.create(params["password"])
         user = params["user"]
@@ -60,7 +92,7 @@ class App < Sinatra::Base
     end
 
     post '/placeholder/logout' do
-        session.destroy
+        session.delete(:user_id)
         redirect "/placeholder"
     end
 
@@ -79,11 +111,38 @@ class App < Sinatra::Base
         redirect "/placeholder"
     end
 
+    # Define a POST route to check the name in the database
+    post '/check_name' do
+        name = params["leader_name"]
+    
+        # Query the database to check if the name exists
+        leader = db.execute("SELECT * FROM leaders WHERE name = ?", name).first
+  
+        if leader.nil?
+            # If name is not found, redirect to a failure or error page
+            @error_message = "Leader not found!"
+            redirect "/placeholder/game"  # Or you can render an error page
+        else
+            # If name is found, trigger the appropriate function
+            # For example, set session or update database, etc.
+            # You can redirect or render based on your needs
+            @leader_found = leader
+            session[:score] += 1
+            redirect "/placeholder/game"  # Redirect to the game with the leader found
+
+        end
+    end
+  
+
     get '/login' do
+        session[:time] = 60
+        session[:score] = 0
         erb :"/login"
     end
 
     post '/login' do
+        session[:time] = 60
+        session[:score] = 0
         username = params['user']
         cleartext_password = params['password'] 
         current_user = db.execute('SELECT * FROM users WHERE user = ?', username).first
@@ -114,6 +173,11 @@ class App < Sinatra::Base
 
         p current_user
         p cleartext_password
-
+    end
+    get '/leaderboard' do
+        session[:time] = 60
+        session[:score] = 0
+        @users = db.execute('SELECT * FROM users ORDER BY score DESC')
+        erb (:"placeholder/leaderboard")
     end
 end
